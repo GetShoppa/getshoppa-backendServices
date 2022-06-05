a2c63e9a43b4b270fe8568a2a1fad7e3fe3ac58c
Eden-Lamr <emmanuelmomodu3@gmail.com>
2022-06-05 20:23:51 +0100

    fixed the res.status on the catch block on createWailist func.

--- controllers/waitListController.js
+++ controllers/waitListController.js
@@ -43,7 +43,7 @@ const createWaitLister = async (req, res) => {
 			message: `${newWaitLister.rows[0].name} added to the waitlist`,
 		});
 	} catch (err) {
-		res.status(404).json({
+		res.status(400).json({
 			status: "fail",
 			message: err.message,
 		});
